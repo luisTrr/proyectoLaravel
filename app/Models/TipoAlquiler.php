7a9f08f47f5e471f6216ff814b37c5a6de3c5b0d
salomon Alfredo Mena Lazarte <salomon.mena@uab.edu.bo>
2023-08-22 23:07:53 -0400

    Se realizo la correccion al crear tipos de alquileres

--- app/Models/TipoAlquiler.php
+++ app/Models/TipoAlquiler.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoAlquiler extends Model
 {
+    protected $table = 'tiposalquiler';
+    protected $fillable = [
+        'tipo', 
+    ];
+    public $timestamps = false;
     use HasFactory;
     public function alquileres()
     {
